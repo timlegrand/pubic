c08f820c97f1647322e0177e1ad909b62a20e99d
Tim Legrand <timlegrand.perso+dev@gmail.com>
2020-01-09 23:23:40 +0100

    feat: add stat information and pretty printing

--- src/pubic/pubic.py
+++ src/pubic/pubic.py
@@ -4,6 +4,7 @@ from pubic import storage
 
 import logging
 import datetime
+import tabulate
 
 
 def _main():
@@ -16,12 +17,27 @@ def _main():
         print(c)
 
     contents = storage.list_container(endpoint, access_token)
-    search_results = [x for x in contents if ".jpg" in x]
-    for r in search_results:
-        print(r)
-        object_data = storage.stat_object(endpoint, access_token, "default", r)
-        print(object_data["Last-Modified"])
-        print(datetime.datetime.fromtimestamp(int(float(object_data["X-Timestamp"]))).strftime('%Y-%m-%d %H:%M:%S'))
+    search_results = [x for x in contents if ".gif" in x]
+    objects_properties = []
+    for i, object_name in enumerate(search_results):
+        object_data = storage.stat_object(endpoint, access_token, "default", object_name)
+        object_last_modified = datetime.datetime.strptime(object_data["Last-Modified"], "%a, %d %b %Y %H:%M:%S %Z")
+        # store_time = datetime.datetime.fromtimestamp(int(float(object_data["X-Timestamp"])))
+        # print(object_last_modified.strftime('%Y-%m-%d %H:%M:%S'))
+        # print(store_time.strftime('%Y-%m-%d %H:%M:%S'))
+        object_size = object_data["Content-Length"]
+        object_type = object_data["Content-Type"]
+        objects_properties.append((
+            object_name,
+            object_last_modified,
+            object_size,
+            object_type
+        ))
+        if i > 10:
+            break
+    print("Search result (only 10 firsts)")
+    headers = ["Name", "Last Modified", "Size (B)", "Type"]
+    print(tabulate.tabulate(objects_properties, headers=headers))
 
 
 if __name__ == '__main__':
