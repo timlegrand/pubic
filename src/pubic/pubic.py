8a9cfe3d0779146add93aef4f46b1cde7c2a7762
Tim Legrand <timlegrand.perso+dev@gmail.com>
2020-01-09 22:32:48 +0100

    feat: add support for listing and stating objects in a container

--- src/pubic/pubic.py
+++ src/pubic/pubic.py
@@ -3,6 +3,7 @@ from pubic import auth
 from pubic import storage
 
 import logging
+import datetime
 
 
 def _main():
@@ -13,6 +14,14 @@ def _main():
     containers = storage.list_containers(endpoint, access_token)
     for c in containers:
         print(c)
+
+    contents = storage.list_container(endpoint, access_token)
+    search_results = [x for x in contents if ".jpg" in x]
+    for r in search_results:
+        print(r)
+        object_data = storage.stat_object(endpoint, access_token, "default", r)
+        print(object_data["Last-Modified"])
+        print(datetime.datetime.fromtimestamp(int(float(object_data["X-Timestamp"]))).strftime('%Y-%m-%d %H:%M:%S'))
 
 
 if __name__ == '__main__':
--- src/pubic/storage.py
+++ src/pubic/storage.py
@@ -21,3 +21,41 @@ def list_containers(endpoint, access_token):
         pdb.set_trace()
 
     return response.text.split("\n")
+
+
+def list_container(endpoint, access_token, container_name="default"):
+    logging.info(f"Listing '{container_name}' container...")
+
+    headers = { "X-Auth-Token": f"{access_token}" }
+    logging.debug(headers)
+
+    response = requests.get(
+        endpoint + "/" + container_name,
+        headers=headers)
+    logging.debug(response.status_code)
+
+    if response.status_code != 200:
+        logging.debug(response.reason)
+        logging.debug(response.text)
+        import pdb
+        pdb.set_trace()
+
+    return response.text.split("\n")
+
+
+def stat_object(endpoint, access_token, container_name="default", object_path=""):
+    logging.debug(f"Stating '{container_name}/{object_path}'...")
+
+    headers = { "X-Auth-Token": f"{access_token}" }
+    logging.debug(headers)
+
+    response = requests.head(
+        endpoint + "/" + container_name + "/" + object_path,
+        headers=headers)
+    logging.debug(response.status_code)
+
+    if response.status_code != 200:
+        logging.debug(response.reason)
+        logging.debug(response.text)
+
+    return response.headers
