c689b49eb977b737b2c0d9f0f6f9ed11d3b2e12c
Tim Legrand <timlegrand.perso+dev@gmail.com>
2020-01-22 22:56:46 +0100

    feat: enable support for '--list-containers' and '--search' CLI options

--- src/pubic/pubic.py
+++ src/pubic/pubic.py
@@ -9,26 +9,40 @@ from pubic._version import __version_text__
 
 
 def _main():
-    parser = argparse.ArgumentParser(
-        description='''A terminal-based GUI client for Git.''')
-    parser.add_argument(
-        '-v', '--version', action='version', version=__version_text__)
-    parser.parse_args()
+    # Handle CLI arguments
+    parser = argparse.ArgumentParser(description="""Pubic, the Hubic cloud storage client made reliable.""")
+    parser.add_argument("-v", "--version", action="version", version=__version_text__)
+    parser.add_argument("--list-containers", action="store_true", default=False, help="list containers")
+    parser.add_argument("--limit", type=int, default=10, help="limit for search results (default 10, 0 means no limit)")
+    parser.add_argument("--search", dest="search_expr",  help="search for files matching the given expression")
+    args = parser.parse_args()
 
-    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+    # Setup logging
+    logging.basicConfig(format="[%(levelname)s] %(message)s", level=logging.INFO)
 
+    # Authenticate against Hubic APIs
     access_token, endpoint = auth.get_storage_credentials()
 
-    containers = storage.list_containers(endpoint, access_token)
-    for c in containers:
-        print(c)
+    if args.list_containers:
+        containers = storage.list_containers(endpoint, access_token)
+        for c in containers:
+            print(c)
 
-    objects = storage.list_container(endpoint, access_token)
-    search_results = [x for x in objects if ".gif" in x][:10]
-
-    objects_properties = storage.stat_object_list(search_results, endpoint, access_token, container_name="default")
-    headers = ["Name", "Last Modified", "Size (B)", "Type"]
-    print(tabulate.tabulate(objects_properties, headers=headers))
+    if args.search_expr:
+        objects = storage.list_container(endpoint, access_token)
+        search_results = [x for x in objects if args.search_expr in x]
+        if args.limit:
+            search_results = [x for x in objects if args.search_expr in x][:args.limit]
+        objects_properties = storage.stat_object_list(
+            search_results,
+            endpoint,
+            access_token,
+            container_name="default")
+        if objects_properties:
+            headers = ["Name", "Last Modified", "Size (B)", "Type"]
+            print(tabulate.tabulate(objects_properties, headers=headers))
+        else:
+            print("No result.")
 
 
 if __name__ == '__main__':
