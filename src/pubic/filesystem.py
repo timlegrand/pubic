e434a151599cc5226d004575ee589aac56cc72da
Tim Legrand <timlegrand.perso+dev@gmail.com>
2020-02-15 09:09:57 +0100

    fix(filesystem): restore dir path comparison for cloud objects

--- src/pubic/filesystem.py
+++ src/pubic/filesystem.py
@@ -6,6 +6,6 @@ def list_files(path):
     local_tree = os.walk(path)
     for dirpath, dirnames, filenames in local_tree:
         for dirname in dirnames:
-            yield (os.path.join(dirpath, dirname) + "/")[len(path) + 1:]
+            yield (os.path.join(dirpath, dirname))[len(path) + 1:]
         for filename in filenames:
             yield (os.path.join(dirpath, filename))[len(path) + 1:]
